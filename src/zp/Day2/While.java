89d9306678d920041926bc82ef979f2dc0115e6d
Your Name <Your email>
2020-06-30 23:34:26 +0800

    do while . while

--- src/zp/Day2/While.java
+++ src/zp/Day2/While.java
@@ -2,10 +2,11 @@ package zp.Day2;
 
 public class While {
     public static void main(String[] args) {
-            int a = 1;
-            while ( a <= 5){
-                System.out.println(" a = " + a);
-                a++;
+            int[]  a = { 1 , 2 , 3 , 4 , 5 };
+            int i = 0 ;
+            while ( i < a.length ){
+                System.out.println( a[i] );
+                i++;
             }
 
     }
--- src/zp/Day2/While2.java
+++ src/zp/Day2/While2.java
@@ -2,11 +2,12 @@ package zp.Day2;
 
 public class While2 {
     public static void main(String[] args){
-        int a = 0;
+        int[] a = { 1 , 2 , 3 , 4 , 5 };
+        int i = 0;
         do {
-            a++;
-            System.out.println(" a = " + a);
-        } while (a <= 4);
+            System.out.println( a[i] );
+            i++;
+        } while ( i<a.length );
 
     }
 }
